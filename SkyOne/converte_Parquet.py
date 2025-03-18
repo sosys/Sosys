0a82fb401fbc4f5a2a5b0b73df3a90c2448b2642
Guilherme Barrios <guilherme.barrios@sosys.com.br>
2025-03-18 15:22:31 -0300

    a

--- SkyOne/converte_Parquet.py
+++ SkyOne/converte_Parquet.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def csv_to_parquet(csv_file: str, parquet_file: str):
     try:
-        df = pd.read_csv(csv_file, delimiter=";", encoding="utf-8")
+        df = pd.read_csv(csv_file, delimiter=";", encoding="utf-8", low_memory=False)
         df.to_parquet(parquet_file, engine='pyarrow', index=True)
         print(f"Arquivo Parquet salvo em: {parquet_file}")
         
@@ -11,13 +11,29 @@ def csv_to_parquet(csv_file: str, parquet_file: str):
     except Exception as e:
         print(f"Erro ao converter CSV para Parquet: {e}" + ' - ' + csv_file)
 
+
 path_de = "C:\\bla\\CSV\\"
 path_para = "C:\\bla\\Parquet\\"
-nome_csv_order = 'Order_Header_tst.csv'
-nome_parquet_order = "order_header_tst.parquet"
+
+nome_csv_order = 'Order_Header.csv'
+nome_parquet_order = "order_header.parquet"
+
 nome_csv_item = 'Order_Item.csv'
 nome_parquet_item = "order_item.parquet"
 
+nome_csv_wallet = 'wallet.csv'
+nome_parquet_wallet = "wallet.parquet"
+
+nome_csv_client = 'client.csv'
+nome_parquet_client = "client.parquet"
+
+nome_csv_product = 'product_sb1.csv'
+nome_parquet_product = "product_sb11.parquet"
+
 # Exemplo de uso
-csv_to_parquet(path_de + nome_csv_order, path_para + nome_parquet_order)
+#csv_to_parquet(path_de + nome_csv_order, path_para + nome_parquet_order)
 #csv_to_parquet(path_de + nome_csv_item, path_para + nome_parquet_item)
+
+#csv_to_parquet(path_de + nome_csv_wallet, path_para + nome_parquet_wallet)
+#csv_to_parquet(path_de + nome_csv_client, path_para + nome_parquet_client)
+csv_to_parquet(path_de + nome_csv_product, path_para + nome_parquet_product)
